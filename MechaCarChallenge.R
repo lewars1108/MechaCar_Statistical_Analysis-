cd3d9cbbca0039d2b7e340a2b5494782e8f0879a
Erika L <erika.lewars@gmail.com>
2022-04-19 19:45:08 -0400

    Deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,18 @@
+#Deliverable 1
 library(tidyverse)
 mechacar_df <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar_df) #generate multiple linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar_df)) #generate multiple linear regression model summary
+
+#Deliverable 2
+suspension_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+total_summary <- suspension_table %>% summarize(Mean=mean(PSI),
+                                          Median=median(PSI),
+                                          Variance=var(PSI),
+                                          SD=sd(PSI), .groups = 'keep') 
+
+lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),
+                                                Median=median(PSI),
+                                                Variance=var(PSI),
+                                                SD=sd(PSI), .groups = 'keep')
